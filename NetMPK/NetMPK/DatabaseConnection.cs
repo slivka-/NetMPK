862b8fa11c81749faa546f1ec175638c8a831ea0
Irozin <irozin7@gmail.com>
2015-12-16 17:57:31 +0100

    Add DatabaseConnection

---
+++ NetMPK/NetMPK/DatabaseConnection.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+
+namespace NetMPK
+{
+    public class DatabaseConnection
+    {
+        private SqlConnection sqlConnection;
+        private static DatabaseConnection databaseConnection;
+
+        private DatabaseConnection() { }
+
+        public static DatabaseConnection getInstance()
+        {
+            if (databaseConnection == null)
+            {
+                databaseConnection = new DatabaseConnection();
+            }
+            return databaseConnection;
+        }
+
+        public bool OpenConnection(String connection = "MPKDB")
+        {
+            try
+            {
+                if (sqlConnection.State.ToString() != "Open")
+                {
+                    sqlConnection = new SqlConnection(@WebConfigurationManager.ConnectionStrings[connection].ToString());
+                    sqlConnection.Open();
+                }
+                return true;
+            }
+            catch (SqlException e)
+            {
+                return false;
+            }
+        }
+
+        public bool CloseConnection()
+        {
+            try
+            {
+                sqlConnection.Close();
+                return true;
+            }
+            catch (SqlException e)
+            {
+                return false;
+            }
+        }
+
+        private List<String> GetOneColumnData(String query, String columnName)
+        {
+            List<String> values = new List<string>();
+
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
+
+            using (SqlDataReader rdr = cmd.ExecuteReader())
+            {
+                while (rdr.Read())
+                {
+                    values.Add(Convert.ToString(rdr[columnName]));
+                }
+            }
+
+            return values;
+        }
+
+        public List<String> GetLinesNumbers()
+        {
+            return GetOneColumnData("Select Line_number FROM Line", "Line_number");
+        }
+
+        public List<String> GetLinesStopNames()
+        {
+            return GetOneColumnData("Select Name FROM Line", "Name");
+        }
+
+    }
+}
